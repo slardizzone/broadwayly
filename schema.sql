1b5a23feb13b56b0d064770022293c2b40b0fa3f
slardizzone <slardizzone@gmail.com>
2013-10-11 14:58:51 -0400

    add broadwayly files

---
+++ schema.sql
@@ -0,0 +1,14 @@
+create table shows (
+	id serial primary key, 
+	title varchar(50) not null, 
+	composer varchar(50) not null, 
+	img_url varchar(400) not null
+);
+ 
+
+create table songs (
+	id serial primary key, 
+	title varchar(50) not null, 
+	embed_url varchar(300) not null,
+	show_id integer references shows(id)
+);
